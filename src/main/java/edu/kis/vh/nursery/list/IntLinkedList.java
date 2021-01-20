6df70b7f1f741fb87cab72502cac9b8b9fc25c1f
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-01-20 12:28:20 +0100

    1.5.5 1.5.6 dodanie diagramu xml oraz JAVADOC

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,22 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ *  Default clas
+ */
 public class IntLinkedList {
-
+	/**
+	 * const variable to return
+	 */
 	private static final int RETURN_EMPTY = -1;
+	/**
+	 * Last node
+	 */
 	private Node last;
 
+	/**
+	 * Metohd to push to list
+	 * @param i value element to push
+	 */
 	public void push(int i) {
 		if (last == null)
 			last = new Node(i);
@@ -15,20 +27,36 @@ public class IntLinkedList {
 		}
 	}
 
+	/**
+	 * method to check list is empty
+	 * @return true is not empty
+	 */
 	public boolean isEmpty() {
 		return last == null;
 	}
 
+	/**
+	 * method to check list is full
+	 * @return false is not full
+	 */
 	public boolean isFull() {
 		return false;
 	}
 
+	/**
+	 * get last value from list
+	 * @return last input value
+	 */
 	public int top() {
 		if (isEmpty())
 			return RETURN_EMPTY;
 		return last.value;
 	}
 
+	/**
+	 * get last value and remove from list
+	 * @return
+	 */
 	public int pop() {
 		if (isEmpty())
 			return RETURN_EMPTY;
@@ -36,11 +64,25 @@ public class IntLinkedList {
 		last = last.prev;
 		return ret;
 	}
+
+	/**
+	 * private class in IntLinkedList
+	 */
 	private class Node {
 
+		/**
+		 * value node
+		 */
 		protected final int value;
+		/**
+		 * neighbour
+		 */
 		protected Node prev, next;
 
+		/**
+		 * set value node
+		 * @param i value
+		 */
 		protected Node(int i) {
 			value = i;
 		}
